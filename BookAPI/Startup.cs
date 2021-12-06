972c6749b2592b5060b0e068a863edec6d862524
Hamroliyev <90793925+Hamroliyev@users.noreply.github.com>
2021-12-06 23:53:12 +0300

    Add services.AddScoped<IBookRepository, BookRepository>(); to Startup ConfigureService

--- BookAPI/Startup.cs
+++ BookAPI/Startup.cs
@@ -1,4 +1,5 @@
 using BookAPI.Models;
+using BookAPI.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -28,6 +29,7 @@ namespace BookAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IBookRepository, BookRepository>();
             services.AddDbContext<BookContext>(o => o.UseSqlite("Data source=books.db"));
             services.AddControllers();
             services.AddSwaggerGen(c =>
