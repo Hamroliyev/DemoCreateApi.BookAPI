2998e23d7cf80bb4e0291a90fa6761d8c5c7f76a
Hamroliyev <90793925+Hamroliyev@users.noreply.github.com>
2021-12-07 00:22:10 +0300

    BookController is full

--- BookAPI/Controllers/BooksController.cs
+++ BookAPI/Controllers/BooksController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BookAPI.Models;
+using BookAPI.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,5 +13,49 @@ namespace BookAPI.Controllers
     [ApiController]
     public class BooksController : ControllerBase
     {
+        private readonly IBookRepository _bookRepository;
+
+        public BooksController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<Book>> GetBooks()
+        {
+            return await _bookRepository.Get();
+        }   
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBooks(int id)
+        {
+            return await _bookRepository.Get(id);
+        }
+        [HttpPost]
+        public async Task<ActionResult<Book>> PostBooks([FromBody] Book book)
+        {
+            var newBook = await _bookRepository.Create(book);
+            return CreatedAtAction(nameof(GetBooks), new { id = newBook.ID }, newBook);
+        }
+        [HttpPut]
+        public async Task<ActionResult> PutBooks(int id,[FromBody] Book book)
+        {
+            if(id!=book.ID)
+            {
+                return BadRequest();
+            }
+            await _bookRepository.Update(book);
+
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var bookToDelete =await _bookRepository.Get(id);
+            if(bookToDelete == null)
+            {
+                return NotFound();
+            }
+            await _bookRepository.Delete(bookToDelete.ID);
+            return NoContent();
+        }
     }
 }
