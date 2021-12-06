3237d4208682d7af92eecb5925a1b2450db2ad8a
Hamroliyev <90793925+Hamroliyev@users.noreply.github.com>
2021-12-06 23:38:39 +0300

    IBookRepository interface is Full

--- BookAPI/Repositories/IBookRepository.cs
+++ BookAPI/Repositories/IBookRepository.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using BookAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BookAPI.Repositories
 {
-    interface IBookRepository
+    public interface IBookRepository
     {
+        Task<IEnumerable<Book>> Get();
+        Task<Book> Get(int id);
+        Task<Book> Create(Book book);
+        Task Update(Book book);
+        Task Delete(int id);
     }
 }
