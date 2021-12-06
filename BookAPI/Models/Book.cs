050162e680167042eddf436111c9c1f3bc8b0c07
Hamroliyev <90793925+Hamroliyev@users.noreply.github.com>
2021-12-06 23:21:07 +0300

    Full a Book Models

--- BookAPI/Models/Book.cs
+++ BookAPI/Models/Book.cs
@@ -7,5 +7,9 @@ namespace BookAPI.Models
 {
     public class Book
     {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
     }
 }
